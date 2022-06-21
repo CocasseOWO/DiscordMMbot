6bc1b53cdb1dcbf48e9af1e9914e9f2660c51f23
CocasseOWO <103246783+CocasseOWO@users.noreply.github.com>
2022-06-21 12:27:50 +0200

    added sell all and clan fight

--- main.py
+++ main.py
@@ -9,16 +9,28 @@ header = {
 
 for i in range (100000):
     repair = repair + 1
-    if repair == 30:
+    if repair == 5:
         payload = {
             'content': "m!repair"
         }
         r = requests.post('CHANNEL', data=payload, headers=header)
+        time.sleep(3)
+        payload = {
+            'content': "m!sell all"
+        }
+        r = requests.post('CHANNEL', data=payload, headers=header)
+        payload = {
+            'content': "m!clan fight"
+        }
+        r = requests.post('CHANNEL', data=payload, headers=header)
+
         repair = repair - repair
+
 
     else:
         payload = {
             'content': "m!m"
         }
         r = requests.post('CHANNEL', data=payload, headers=header)
-    time.sleep(40)
+    time.sleep(3)
+    print(i)
