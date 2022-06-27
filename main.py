ca84f99dfb623b41d62125964c78f4f6903c9bdf
CocasseOWO <103246783+CocasseOWO@users.noreply.github.com>
2022-06-27 13:29:55 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,36 +3,15 @@ import time
 
 repair = 0
 
-channel = "CHANNEL"
+channel = "CHANNEL_HERE"
 
 header = {
-    'authorization': 'TOKEN'
+    'authorization': 'TOKEN_HERE'
 }
 
 for i in range (100000):
-    repair = repair + 1
-    if repair == 30:
-        payload = {
-            'content': "m!repair"
-        }
-        r = requests.post(channel, data=payload, headers=header)
-        time.sleep(3)
-        payload = {
-            'content': "m!sell all"
-        }
-        r = requests.post(channel, data=payload, headers=header)
-        payload = {
-            'content': "m!clan fight"
-        }
-        r = requests.post(channel, data=payload, headers=header)
-
-        repair = repair - repair
-
-
-    else:
-        payload = {
-            'content': "m!m"
-        }
-        r = requests.post(channel, data=payload, headers=header)
+    payload = {
+        'content': "m!m"
+    }
+    r = requests.post(channel, data=payload, headers=header)
     time.sleep(3)
-    print(i)
